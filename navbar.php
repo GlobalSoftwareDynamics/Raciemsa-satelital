9749bf010d9c50c00d9d0a678367b8c6b31bbf8d
Joaquin Rivera Lopez <joaquin.rivera@gsdynamics.com>
2017-12-02 18:02:31 -0500

    Add files via upload

--- navbar.php
+++ navbar.php
@@ -13,7 +13,7 @@
                         </a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="Dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Búsqueda de Dts
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
